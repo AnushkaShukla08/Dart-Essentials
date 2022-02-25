1417c3845826cb082229daa8f47b1a65b24fb73f
AnushkaShukla08 <89094653+AnushkaShukla08@users.noreply.github.com>
2022-02-25 21:31:36 +0530

    Update Question4.dart

--- Task1/Question4.dart
+++ Task1/Question4.dart
@@ -1,16 +1,23 @@
 import 'dart:io';
 
-void evenodd(int num) {
-  if (num % 2 == 0) {
-    print("$num is even");
-  } else {
-    print("$num is odd");
-  }
-}
+void addition(int a, int b) => print(a + b);
+void sub(int a, int b) => print(a - b);
+void mul(int a, int b) => print(a * b);
+void div(int a, int b) => print(a / b);
 
 void main() {
-  print("Enter number to check even or odd");
+  print("Enter number ");
 
-  int? n = int.parse(stdin.readLineSync()!);
-  evenodd(n);
+  int? n1 = int.parse(stdin.readLineSync()!);
+  print("Enter number ");
+
+  int? n2 = int.parse(stdin.readLineSync()!);
+  stdout.write("$n1 + $n2 =");
+  addition(n1, n2);
+  stdout.write("$n1 - $n2 =");
+  sub(n1, n2);
+  stdout.write("$n1 * $n2 =");
+  mul(n1, n2);
+  stdout.write("$n1 / $n2 =");
+  div(n1, n2);
 }
