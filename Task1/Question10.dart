33629116ca883483ac90238a8111bdc76267c301
AnushkaShukla08 <89094653+AnushkaShukla08@users.noreply.github.com>
2022-02-25 20:15:49 +0530

    Create Question10.dart

---
+++ Task1/Question10.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  int i, j, r, num;
+  num = 7;
+
+  for (r = 1; r <= 7; r++) {
+    for (i = 1; i <= num; i++) {
+      print("  ");
+    }
+    for (j = 1; j <= r; j++) {
+      stdout.write("* ");
+    }
+    print("\n");
+  }
+}
