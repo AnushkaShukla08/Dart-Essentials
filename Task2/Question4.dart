4eab444c142d958eb126b8cc7e8958a16ec32ba0
AnushkaShukla08 <89094653+AnushkaShukla08@users.noreply.github.com>
2022-02-26 20:45:15 +0530

    Update Question4.dart

--- Task2/Question4.dart
+++ Task2/Question4.dart
@@ -1,37 +1,14 @@
 import 'dart:io';
+import 'dart:convert';
 
 void main() {
-  var details = {
-    1: 'a', 
-    2: 'b',
-    3: 'c',
-    4: 'd',
-    5: 'e',
-    6: 'f',
-    7: 'g',
-    8: 'h',
-    9: 'i',
-    10: 'j',
-    11: 'k',
-    12: 'k',
-    13: 'l',
-    14: 'm',
-    15: 'n',
-    16: 'o',
-    17: 'p',
-    18: 'q',
-    19: 'r',
-    20: 's',
-    21: 't',
-    22: 'u',
-    23: 'v',
-    24: 'w',
-    25: 'x',
-    26: 'z'
-  };
-  //print(details);
+  int i;
+  //var alpha;
+  List<int> numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26 ];
+  var alpha = numbers.map((num) => ascii.decode([96 + num])); 
+ 
   stdout.write('enter number from 1-26 :');
   int? n = int.parse(stdin.readLineSync()!);
+
   stdout.write('The corresponding letter to $n :');
-  print(details[n]);
-}
+  print(ascii.decode([96 + n]));
