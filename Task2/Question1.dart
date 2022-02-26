4fbda30330e72c28cbe50f9abb33f64023258379
AnushkaShukla08 <89094653+AnushkaShukla08@users.noreply.github.com>
2022-02-26 14:43:44 +0530

    Create Question1.dart

---
+++ Task2/Question1.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+void main()
+{
+  print('1 (i)--c');
+  print('1 (ii)--2');
+  print('1 (iii)--3');
+  print('1 (iv)--1');
+  print('1 (v)--3');
+  print('1 (vi)--2');
+  print('1 (vii)--1,2,4');
+  
+}
